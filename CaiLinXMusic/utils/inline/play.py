7be31da2adf6ff95b38cc83777724702a36b916b
maythushar-music <maythusharofficial@gmail.com>
2025-01-30 09:52:42 +0630

    Update play.py

--- CaiLinXMusic/utils/inline/play.py
+++ CaiLinXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/myqueenmedusaofficial"
+                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/ZuriOfficialBoy"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version10"
+                text=" ᴄʜᴀᴛ ", url=f"https://t.me/ZuriOfficialUpdate"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/myqueenmedusaofficial"
+                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/ZuriOfficialBoy"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/seriousvs_version10"
+                text=" ᴄʜᴀᴛ ", url=f"https://t.me/ZuriOfficialUpdate"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
