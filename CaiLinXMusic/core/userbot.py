83e2bf243f304eb6eca167f951fa04799f97625f
maythushar-music <maythusharofficial@gmail.com>
2025-01-30 09:50:40 +0630

    Update userbot.py

--- CaiLinXMusic/core/userbot.py
+++ CaiLinXMusic/core/userbot.py
@@ -71,8 +71,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("seriousvs_version10")
-                await self.one.join_chat("seriousvs_version20")
+                await self.one.join_chat("ZuriOfficialUpgrade")
+                await self.one.join_chat("ZuriOfficialUpdate")
 
             except:
                 pass
@@ -99,8 +99,8 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("seriousvs_version10")
-                await self.two.join_chat("seriousvs_version20")
+                await self.two.join_chat("ZuriOfficialUpgrade")
+                await self.two.join_chat("ZuriOfficialUpdate")
             except:
                 pass
             assistants.append(2)
@@ -121,8 +121,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("seriousvs_version10")
-                await self.three.join_chat("seriousvs_version20")
+                await self.three.join_chat("ZuriOfficialUpgrade")
+                await self.three.join_chat("ZuriOfficialUpdate")
             except:
                 pass
             assistants.append(3)
@@ -142,8 +142,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("seriousvs_version10")
-                await self.four.join_chat("seriousvs_version20")
+                await self.four.join_chat("ZuriOfficialUpgrade")
+                await self.four.join_chat("ZuriOfficialUpdate")
             except:
                 pass
             assistants.append(4)
@@ -163,8 +163,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("seriousvs_version10")
-                await self.five.join_chat("seriousvs_version20")
+                await self.five.join_chat("ZuriOfficialUpgrade")
+                await self.five.join_chat("ZuriOfficialUpdate")
             except:
                 pass
             assistants.append(5)
