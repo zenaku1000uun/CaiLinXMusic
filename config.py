caeef6e92e47401627f32e27ad2980abc439a5a2
maythushar-music <maythusharofficial@gmail.com>
2025-02-02 17:36:15 +0630

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ZuriOfficialUpgrade")
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
-AUTO_GCAST = os.getenv("AUTO_GCAST")
+AUTO_GCAST = os.getenv("AUTO_GCAST", "on")
 
 # Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
 AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
