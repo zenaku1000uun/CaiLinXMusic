660c49f39be21271e3b211ac70bd05d4b78b0cfe
maythushar-music <maythusharofficial@gmail.com>
2025-02-03 15:22:52 +0630

    Update config.py

--- config.py
+++ config.py
@@ -54,6 +54,9 @@ AUTO_GCAST = os.getenv("AUTO_GCAST", "on")
 # Auto Broadcast Message That You Want Use In Auto Broadcast In All Groups.
 AUTO_GCAST_MSG = getenv("AUTO_GCAST_MSG", "")
 
+# Token allow for access youtube songs. [ True = use token data, False = use cookies ]
+TOKEN_ALLOW = os.getenv("TOKEN_ALLOW", "False")
+
 # Get this credentials from https://developer.spotify.com/dashboard
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", "bcfe26b0ebc3428882a0b5fb3e872473")
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", "907c6a054c214005aeae1fd752273cc4")
