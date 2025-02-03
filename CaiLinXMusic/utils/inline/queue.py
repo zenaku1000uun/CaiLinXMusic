bb98d7cfa73847b1fc5be910bf048aa0b99c6f66
maythushar-music <maythusharofficial@gmail.com>
2025-02-03 15:16:56 +0630

    Update queue.py

--- CaiLinXMusic/utils/inline/queue.py
+++ CaiLinXMusic/utils/inline/queue.py
@@ -71,15 +71,6 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
-        [
-            InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/ZuriOfficialBoy"
-            ),
-            InlineKeyboardButton(
-                text=" ꜱᴜᴘᴘᴏʀᴛ ", url=f"https://t.me/ZuriOfficialUpdate"
-            ),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
