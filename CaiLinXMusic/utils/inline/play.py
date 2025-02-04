b5aafcbdd281feffe01bbfb4a8f3b23a58258beb
maythushar-music <maythusharofficial@gmail.com>
2025-02-04 13:52:09 +0630

    Update play.py

--- CaiLinXMusic/utils/inline/play.py
+++ CaiLinXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/ZuriOfficialBoy"
+                text=" ပိုင်ရှင် ", url=f"https://t.me/ZuriOfficialBoy"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/ZuriOfficialUpdate"
+                text=" ပိုမိုသိရှိရန် ", url=f"https://t.me/ZuriOfficialUpdate"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text=" ᴏᴡɴᴇʀ ", url=f"https://t.me/ZuriOfficialBoy"
+                text=" ပိုင်ရှင် ", url=f"https://t.me/ZuriOfficialBoy"
             ),
             InlineKeyboardButton(
-                text=" ᴄʜᴀᴛ ", url=f"https://t.me/ZuriOfficialUpdate"
+                text=" ပိုမိုသိရှိရန် ", url=f"https://t.me/ZuriOfficialUpdate"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
