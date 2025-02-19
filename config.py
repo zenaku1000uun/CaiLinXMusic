85c21fdaab8dd8d5e4b79d5a4e43d25f235bd14d
maythushar-music <maythusharofficial@gmail.com>
2025-02-19 19:14:16 +0630

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,12 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ZuriOfficialUpdate")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/ZuriOfficialUpgrade")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
-AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
+
+# Time after which you're assistant account will leave chats automatically.
+AUTO_LEAVE_ASSISTANT_TIME = int(
+    getenv("ASSISTANT_LEAVE_TIME", 25200)
+)  # Remember to give value in Seconds
 
 # Auto Gcast/Broadcast Handler (True = broadcast on , False = broadcast off During Hosting, Dont Do anything here.)
 AUTO_GCAST = os.getenv("AUTO_GCAST", "off")
