09d77f3edd88d243fc62c3e6b550ee657a188c95
zenaku1000uun <cai.l.i.n.5.u.sm@gmail.com>
2025-03-05 08:54:36 +0630

    Update start.py

--- CaiLinXMusic/utils/inline/start.py
+++ CaiLinXMusic/utils/inline/start.py
@@ -33,7 +33,7 @@ def private_panel(_):
             InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_7"], callback_data="gib_source")
+            InlineKeyboardButton(text=_["S_B_7"], callback_data="ᴍʏ_ꜱᴛᴏʀʏ")
         ],
     ]
     return buttons
